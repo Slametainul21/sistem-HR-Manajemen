b769fe1c1349e024a42fe01848f920ea5bcf2f9f
Fitro776 <fitropraaidinza776@gmail.com>
2025-03-18 15:55:34 +0700

    Change Register button's text color

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -16,7 +16,7 @@
                         <a class="nav-link btn btn-outline-primary mx-2" href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link btn btn-primary text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        <a class="nav-link btn btn-outline-primary mx-2" href="{{ route('register') }}">{{ __('Register') }}</a>
                     </li>
                 @else
                     <li class="nav-item dropdown">
