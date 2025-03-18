e8f33003f2c4c6c74d8073ec614807777ea33db6
Fitro776 <fitropraaidinza776@gmail.com>
2025-03-19 03:48:53 +0700

    Fixes Register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:tbl_users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'role_id' => ['required', 'string', 'in:0,1'],
             'department_id' => ['required', 'exists:tbl_departments,id'],
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
-    protected $table = 'tbl_users';
+    protected $table = 'tbl_users'; // Specify the correct table name
 
     protected $fillable = [
         'name', 'email', 'password', 'role_id', 'department_id'
