6c6df740afcd53d3d3a30d0055383fb1e0ded5aa
Slametainul21 <slametainul09@gmail.com>
2025-03-25 12:21:23 +0700

    forgot-password-Fixed

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -17,7 +17,7 @@ class ForgotPasswordController extends Controller
 {
     public function showForgetPasswordForm()
     {
-        return view('auth.forgot_password');
+        return view('auth.forgot-password');
     }
 
     public function submitForgetPasswordForm(Request $request)
